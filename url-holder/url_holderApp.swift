96c06aa1b95a6689be624b64b95a4f7cfe63eb3d
cut0 <zeron9421@gmail.com>
2022-01-30 14:08:43 +0900

    Initial Commit

---
+++ url-holder/url_holderApp.swift
@@ -0,0 +1,17 @@
+//
+//  url_holderApp.swift
+//  url-holder
+//
+//  Created by Rei Kato on 2022/01/30.
+//
+
+import SwiftUI
+
+@main
+struct url_holderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
